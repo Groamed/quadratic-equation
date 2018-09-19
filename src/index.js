1c910f724759cc8320e202a143f85dd1f9ed0dc2
Павел Тамилин <you@example.com>
2018-09-19 22:57:25 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var a = parseInt(equation.slice(0, equation.indexOf(' ')));
+  var b = parseInt((equation.slice(equation.indexOf('^2') + 2, equation.indexOf('x ') - 3)).replace(/\s/g, ''));
+  var c = parseInt((equation.slice(equation.indexOf('x ') + 2, equation.length)).replace(/\s/g, ''));
+  var sdiscr = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+  return [Math.round((-b-sdiscr)/(2*a)), Math.round((-b+sdiscr)/(2*a))].sort((a, b) => a - b)
 }
